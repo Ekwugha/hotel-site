c63b4057739b225921165c22bb23f34765f0b7e2
Ekwugha <elloekwugha@gmail.com>
2021-09-21 14:15:37 +0100

    update on the navbar of the landing page

---
+++ script/script.js
@@ -0,0 +1,15 @@
+// const navbar = document.querySelector('.navbar');
+// // const a = navbar.style.backgroundColor = 'white';
+// window.addEventListener('scroll', function() {
+//     const scrollHeight = window.pageYOffset;
+//     // const height = window.pageXOffset().height;
+//     const navHeight = navbar.getBoundingClientRect().height;
+//     if (scrollHeight > navHeight) {
+//         navbar.classList.add('fixed-top');
+//         navbar.style.backgroundColor = 'white';
+//         navbar.style.color = 'black';
+//     } else {
+//     navbar.classList.remove('fixed-top');
+//     navbar.style.color = 'white';
+//     } 
+// })
